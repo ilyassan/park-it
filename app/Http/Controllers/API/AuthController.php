fe8301c47596503e6b935a4aaec5c337927b02e1
ilyass anida <anidailyass@gmail.com>
2025-03-10 14:02:57 +0000

    feat: (PAR-6) Develop the Login funcionality.

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,13 +5,13 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     
-
     public function signup(Request $request)
     {
         try {
@@ -50,4 +50,44 @@ class AuthController extends Controller
         }
     }
 
+    public function login(Request $request)
+    {
+        try {
+            $validatedData = Validator::make($request->all(), [
+                "email" => "required|email",
+                "password" => "required",
+            ]);
+
+            if ($validatedData->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validatedData->errors(),
+                ], 401);
+            }
+
+            if (!Auth::attempt($request->only(["email", "password"]))) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email or Password does not match with our record.',
+                ], 401);
+            }
+
+
+            $user = User::where('email', $request->email)->first();
+
+            return response()->json([
+                'status' => true,
+                'message' => "User Loggin In Successfully",
+                'token' => $user->createToken("API TOKEN")->plainTextToken,
+            ], 200);
+
+        } catch (\Throwable $th) {
+            
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
 }
