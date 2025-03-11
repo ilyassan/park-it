6787a112dc9cf5024bb25b179304dbb662a5a15c
ilyass anida <anidailyass@gmail.com>
2025-03-11 15:31:32 +0000

    feat: (PAR-25) Schelduling the status change of the reservations.

--- routes/console.php
+++ routes/console.php
@@ -1,10 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Console\ClosureCommand;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Models\Reservation;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    /** @var ClosureCommand $this */
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::call(function () {
+    Reservation::where("end_date", "<", now())->update(["status" => "completed"]);
+})->everyFifteenMinutes();
